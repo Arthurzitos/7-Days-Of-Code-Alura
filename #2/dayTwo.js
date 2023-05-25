fcb4b35546021e28a6b78a765866029375987c80
xArthur29 <arthurgruimaraes@gmail.com>
2023-05-25 18:57:18 -0300

    Resolução do exercício proposto

---
+++ #2/dayTwo.js
@@ -0,0 +1,5 @@
+var nome = prompt("Qual seu nome? ");
+var idade = prompt("Qual seu idade? ");
+var linguagem = prompt("Qual linguagem você está estudando? ");
+
+alert("Olá " + [nome] + ", você tem " + [idade] + " anos e está aprendendo " + [linguagem] + ", que legal!");
